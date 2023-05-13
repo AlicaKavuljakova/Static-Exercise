3961fb462db418587ba51eb87c89219ea9bbe66d
alica kavuljakova <drobeek@gmail.com>
2023-05-13 15:53:45 -0700

    homework

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace StaticExercise
+﻿using System;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("WHat's the temperature in F that you need to convert to C?");
+            var input=int.Parse(Console.ReadLine());
+            Console.WriteLine("The temperature is "+TempConverter.FarenheitToCelsius(input) +" farenheit");
+            Console.WriteLine( "What is the temperature in Celsius that needs converting into Farenheit?");
+            var inputC=int.Parse( Console.ReadLine());
+            Console.WriteLine("The temperature is " + TempConverter.CelsiusToFarenheit(inputC) + " Faerenheit");
         }
     }
 }
+/*Now create a static class called TempConverter.The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+
+For reference:*/
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       public static double FarenheitToCelsius(double far)
+            {
+             
+              return (far - 32) /1.8;
+                
+            }
+            public static double CelsiusToFarenheit(double cel)
+            {
+
+                return (cel) * 1.8 + 32;
+
+            }
+        
+    }
+}
+/*FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!*/
